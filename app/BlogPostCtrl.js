5f283b759ee287ac081c957da88f792a588c7154
Dairon Rodriguez <drodriguez@fvi.edu>
2017-03-30 19:51:53 -0400

    completed quiz2

--- app/BlogPostCtrl.js
+++ app/BlogPostCtrl.js
@@ -7,5 +7,27 @@
     }
 
     var module = angular.module("exerciseApp", []);
-    module.controller('BlogPostCtrl', [BlogPostCtrl]);
+module.controller('BlogPostCtrl', ['$scope',function($scope){
+	$scope.info = [
+	{
+		"Id":"1",
+		"Author":"George Simenon",
+		"Title":"Detectives"
+	},
+	{
+		"Id":"2",
+		"Author":"Leo Tolstoy",
+		"Title":"War and Peace"
+	}
+	
+	
+	
+	
+	
+	
+	
+	]
+	
+	
+}]);
 })();
